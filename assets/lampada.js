8498332462ce90c45fec62d21efa9dbb65fa0e5d
viannaingrid <ingrid.vianna1@outlook.com>
2024-06-25 22:25:23 -0300

    atualização

--- assets/lampada.js
+++ assets/lampada.js
@@ -1,1 +1,24 @@
-const turOn = document.getElementById('turnOn')
+const turOn = document.getElementById("turnOn");
+const turOff = document.getElementById("turnOff");
+const lamp = document.getElementById("lamp");
+
+function lampOn(){
+    lamp.src = "/assets/img/ligada.jpg";
+}
+
+function lampOff(){
+    lamp.src = "/assets/img/desligada.jpg";
+}
+
+function lampBroken(){
+    lamp.src = "/assets/img/quebrada.jpg";
+}
+
+turOn.addEventListener("click", lampOn);
+turOff.addEventListener("click", lampOff);
+
+lamp.addEventListener("mouseover", lampOn);
+lamp.addEventListener("mouseleave", lampOff);
+
+lamp.addEventListener("dblclick", lampBroken);
+
